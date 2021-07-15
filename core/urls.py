b4195581c9f4d9f25b8f2a0de242e1a58cabf7ab
minhyeok <sonmh79@naver.com>
2021-07-16 02:37:57 +0900

    veiwsntemplates_ing

---
+++ core/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from rooms import views as room_views
+
+urlpatterns = [path("", room_views.all_rooms, name="home")]
+
+app_name = "core"
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,23 @@
+from math import ceil
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+def all_rooms(request):
+    page = int(request.GET.get("page", 1) or 1)
+    page_size = 10
+    limit = page * page_size
+    start = page_size * (page - 1)
+    all_rooms = models.Room.objects.all()[start:limit]
+    page_count = ceil(models.Room.objects.count() / page_size)
+    page_range = range(1, page_count + 1)
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": page_range,
+        },
+    )
