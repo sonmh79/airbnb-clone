66f8254e85a44dfd0cab6004bc76aa3964b839b2
Minhyeok <sonmh79@naver.com>
2021-08-02 23:31:16 +0900

    50%

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,12 @@
 from django import forms
+from django.db.models import fields
+from django.forms.fields import EmailField
 from . import models
 
 
 class LoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "email"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "password"}))
 
     def clean(self):
         email = self.cleaned_data.get("email")
@@ -12,7 +14,6 @@ class LoginForm(forms.Form):
         try:
             user = models.User.objects.get(email=email)
             if user.check_password(password):
-                print(self.cleaned_data)
                 return self.cleaned_data
             else:
                 self.add_error("password", forms.ValidationError("Password is wrong"))
@@ -20,21 +21,13 @@ class LoginForm(forms.Form):
             self.add_error("email", forms.ValidationError("User does not exist"))
 
 
-class SignUpForm(forms.Form):
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
 
-    first_name = forms.CharField(max_length=20)
-    last_name = forms.CharField(max_length=20)
-    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
     password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(email=email)
-            raise forms.ValidationError("User already exists with that email")
-        except models.User.DoesNotExist:
-            return email
 
     def clean_password1(self):
         password = self.cleaned_data.get("password")
@@ -43,3 +36,11 @@ class SignUpForm(forms.Form):
             raise forms.ValidationError("Password confirmation does not match")
         else:
             return password
+
+    def save(self):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()
--- users/views.py
+++ users/views.py
@@ -7,21 +7,18 @@ from . import forms
 # Create your views here.
 
 
-class LoginView(View):
-    def get(self, request):
-        form = forms.LoginForm()
-        return render(request, "users/login.html", {"form": form})
+class LoginView(FormView):
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
 
 def log_out(request):
@@ -38,3 +35,8 @@ class SignUpView(FormView):
         "last_name": "Son",
         "email": "sonmh79@naver.com",
     }
+
+    def form_valid(self, form):
+        form.save()
+
+        return super().form_valid(form)
