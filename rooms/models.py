4b63eba53ed57b1521c0cc18ed335c7bbdc07470
Minhyeok <sonmh79@naver.com>
2021-09-16 16:04:02 +0900

    UserProfile

--- rooms/models.py
+++ rooms/models.py
@@ -94,7 +94,9 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)  # 모델끼리 연결
+    host = models.ForeignKey(
+        user_models.User, related_name="rooms", on_delete=models.CASCADE
+    )  # 모델끼리 연결
     room_type = models.ForeignKey(
         RoomType, related_name="rooms", on_delete=models.SET_NULL, null=True
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
         "50vh": "50vh",
         "75vh": "75vh",
       },
+      minHeight: {
+        "50vh": "50vh",
+        "75vh": "75vh",
+      },
     },
   },
   variants: {
