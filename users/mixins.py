50b66e82bab0a312b396c67ae7ff10d45aeea25c
Minhyeok <sonmh79@naver.com>
2021-09-18 23:35:04 +0900

    more_mixins

--- users/mixins.py
+++ users/mixins.py
@@ -1,15 +1,29 @@
-from django.shortcuts import redirect, reverse
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.contrib import messages
+from django.urls import reverse_lazy
+
+
+class EmailLoginOnlyView(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.login_method == "email"
+
+    def handle_no_permission(self):
+        messages.error(self.request, "Can't go there")
+        return redirect("core:home")
 
 
 class LoggedOutOnlyView(UserPassesTestMixin):
     """로그아웃인 상태인 사람만 볼 수 있다.(익명의 유저만)"""
-
-    permission_denied_message = "Page not found"
 
     def test_func(self):
         return not self.request.user.is_authenticated
 
     def handle_no_permission(self):
         """로그인된 상태일 떼 이동"""
+        messages.error(self.request, "Can't go there")
         return redirect("core:home")
+
+
+class LoggedinOnlyView(LoginRequiredMixin):
+    login_url = reverse_lazy("users:login")
--- users/views.py
+++ users/views.py
@@ -27,6 +27,13 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
             login(self.request, user)
         return super().form_valid(form)
 
+    def get_success_url(self):
+        next_arg = self.request.GET.get("next")
+        if next_arg is not None:
+            return next_arg
+        else:
+            return reverse("core:home")
+
 
 def log_out(request):
     messages.info(request, f"See you later")
@@ -194,7 +201,7 @@ class UserProfileView(DetailView):
     context_object_name = "user_obj"
 
 
-class UpdateProfileView(SuccessMessageMixin, UpdateView):
+class UpdateProfileView(mixins.LoggedinOnlyView, SuccessMessageMixin, UpdateView):
 
     model = models.User
     template_name = "users/update-profile.html"
@@ -223,7 +230,12 @@ class UpdateProfileView(SuccessMessageMixin, UpdateView):
         return form
 
 
-class UpdatePasswordView(SuccessMessageMixin, PasswordChangeView):
+class UpdatePasswordView(
+    mixins.EmailLoginOnlyView,
+    mixins.LoggedinOnlyView,
+    SuccessMessageMixin,
+    PasswordChangeView,
+):
     template_name = "users/update-password.html"
     success_message = " Password Updated"
 
