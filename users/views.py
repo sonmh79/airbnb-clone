81165644cc17842833305ade6f57dd31e8d26efb
Minhyeok <sonmh79@naver.com>
2021-10-13 02:10:47 +0900

    Session

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import View, FormView, DetailView, UpdateView
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.core.files.base import ContentFile
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
@@ -250,3 +251,12 @@ class UpdatePasswordView(
 
     def get_success_url(self):
         return self.request.user.get_absolute_url()
+
+
+@login_required
+def switch_hosting(request):
+    try:
+        del request.session["is_hosting"]
+    except KeyError:
+        request.session["is_hosting"] = True
+    return redirect(reverse("core:home"))
