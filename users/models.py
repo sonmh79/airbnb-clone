bc1e0ca0d788920a555c54650081b820a473a309
Minhyeok <sonmh79@naver.com>
2021-12-18 16:37:46 +0900

    model translations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _  # 모델 번역 가능
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.shortcuts import reverse
